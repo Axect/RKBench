b3d4f6ee62ff54640c4490161ecc453a307fe36e
Axect <axect@outlook.kr>
2022-06-07 12:37:13 +0900

    Delete Unnecessary files

--- rust/target/release/deps/rust-3ccd83e075b9d110.d
+++
@@ -1,5 +0,0 @@
-/home/kavis/Documents/Project/Bench/RKBench/rust/target/release/deps/rust-3ccd83e075b9d110: src/main.rs
-
-/home/kavis/Documents/Project/Bench/RKBench/rust/target/release/deps/rust-3ccd83e075b9d110.d: src/main.rs
-
-src/main.rs:
--- rust/target/release/rust.d
+++
@@ -1,1 +0,0 @@
-/home/kavis/Documents/Project/Bench/RKBench/rust/target/release/rust: /home/kavis/Documents/Project/Bench/RKBench/rust/src/main.rs
